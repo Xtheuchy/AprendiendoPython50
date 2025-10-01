27e28b7bfae47643696f9853632c34b0f4863a45
Elvis cayllahua Condori <168233293+Xtheuchy@users.noreply.github.com>
2025-10-01 15:20:44 -0500

    Agregando 11.py

---
+++ 11.py
@@ -0,0 +1,18 @@
+#Crea un diccionario con 
+#nombre, edad y ciudad, muéstralo.
+
+persona = {
+    "nombre":"Joaquin",
+    "edad":25,
+    "ciudad":"Lima"
+}
+
+#Mostramos los datos usando claves
+print(f"Nombre: {persona['nombre']}")
+
+print(f"Edad: {persona['edad']}")
+
+print(f"Ciudad: {persona['ciudad']}")
+
+#Imprimir diccionario
+print(persona)
