29b851d176edff748e8e313afed1e34592b6c9c3
Elvis cayllahua Condori <168233293+Xtheuchy@users.noreply.github.com>
2025-10-01 15:20:29 -0500

    Agregando 02.py

---
+++ 02.py
@@ -0,0 +1,10 @@
+#Pide dos números y muestra suma, resta,
+#multiplicación, división y potencia
+num1 = float(input("Ingresa el primer numero: "))
+num2 = float(input("Ingresa el segundo numero: "))
+
+print(f"Suma : {num1 + num2}")
+print(f"Resta : {num1 - num2}")
+print(f"Multiplicación : {num1 * num2}")
+print(f"División : {num1 / num2}")
+print(f"Potencia : {num1 ** num2}")
