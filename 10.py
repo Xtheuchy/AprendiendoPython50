142c2efc65324d599988a3ae250bd2615c812c61
Elvis cayllahua Condori <168233293+Xtheuchy@users.noreply.github.com>
2025-10-01 15:20:42 -0500

    Agregando 10.py

---
+++ 10.py
@@ -0,0 +1,12 @@
+#Crea una tupla y un conjunto 
+#muéstralos en consola
+
+frutas_Tupla = ("Manzana","Platano","Mango")
+
+frutas_Conjunto = {
+    "Manzana", "Platano","Mango",
+    "Manzana"
+}
+
+print(f"Tupla: {frutas_Tupla}")
+print(f"Conjunto {frutas_Conjunto}")
