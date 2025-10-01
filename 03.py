3aa89089732a9801dcc94fe738ad52d4426c8a94
Elvis cayllahua Condori <168233293+Xtheuchy@users.noreply.github.com>
2025-10-01 15:20:31 -0500

    Agregando 03.py

---
+++ 03.py
@@ -0,0 +1,13 @@
+#Pide un número como texto, conviértelo 
+#a entero y decimal, muestra su tipo
+
+numero = input("Ingrese un numero : ")
+print(type(numero))
+
+#Convertir a numero entero
+entero = int(numero)
+print(type(entero))
+
+#Covertir a decimal
+decimal = float(numero)
+print(type(decimal))
