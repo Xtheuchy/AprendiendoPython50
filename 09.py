7a8ba810482a539d53d3ce086f99c26b4f674b09
Elvis cayllahua Condori <168233293+Xtheuchy@users.noreply.github.com>
2025-10-01 15:20:41 -0500

    Agregando 09.py

---
+++ 09.py
@@ -0,0 +1,8 @@
+#Lista de frutas: Crea una lista 
+#de 5 frutas y muéstralas con su posición
+
+Frutas = ["Manzana","Platano","Durazno","Pera","Mango"]
+
+for i in range(len(Frutas)):
+    print(f"{i} - {Frutas[i]}")
+    
